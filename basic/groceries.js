8cbd50cca8ef57d5f00888bfe6f7be3165ada44d
amoore877 <amoore877@gmail.com>
2018-05-02 18:59:45 -0400

    Proof of concept groceries list webtask; essentially uses server to store an array for add/remove/read

--- basic/groceries.js
+++ basic/groceries.js
@@ -1,8 +1,81 @@
 module.exports = function(context, cb) {
-//	cb(null, { i_am: 'done '});
+	//debug
 	console.log(context);
-	console.log(context.body);
-	console.log(context.body.name);
 	
-	cb(null, { hello: context.body.name || 'Anonymous' });
+	var cmd = context.data.cmd;
+	if(cmd == "LIST"){
+		//list out storage
+		console.log("Listing items");
+		context.storage.get(function (error, data) {
+	        if (error) return cb(error);
+	        if(!data){
+	        	//no existing list
+	        	console.log("No existing data");
+	        	return cb(null, "No existing data");
+	        }else{
+	        	//simply return the list
+	        	return cb(null, data);
+	        }
+		});
+	}else if (cmd == "DEL"){
+		//delete
+		console.log("Deleting items");
+		context.storage.get(function (error, data) {
+			if (error) return cb(error);
+			if(!data){
+				//no existing list
+				console.log("No existing data");
+				return cb(null, "No existing data");
+			}else{
+				//loop through deletion list
+				for(var delIndex = 0; delIndex < context.data.items.length; delIndex++){
+					var delItem = context.data.items[delIndex];
+					var matchIndex = data.indexOf(delItem);
+					if(matchIndex == -1){
+						console.log(newItem + " does not exist in list.");
+					}else{
+						console.log("Deleting item: " + delItem);
+						data.splice(matchIndex, 1);
+					}
+				}
+				
+				//all deletions done, persist
+		        context.storage.set(data, function (error) {
+		        	if (error) return cb(error);
+		        	cb(null, "Completed");
+		        });
+			}
+		});
+	}else{
+		//assume put
+		console.log("Putting items");
+		context.storage.get(function (error, data) {
+		        if (error) return cb(error);
+		        //append to existing list
+		        if(!data){
+		        	//no existing list
+		        	console.log("No existing data");
+		        	data = context.data.items;
+		        }else{
+		        	//existing list, add non-duplicates
+		        	for(var newIndex = 0; newIndex < context.data.items.length; newIndex++){
+		        		var newItem = context.data.items[newIndex];
+		        		var newEle = (data.indexOf(newItem) == -1);
+		        		
+		        		if(newEle){
+		        			console.log("Adding new element: " + newItem);
+		        			data.push(newItem);
+		        		}else{
+		        			console.log("Found duplicate: " + newItem);
+		        		}
+		        	}
+		        	
+		        }
+		        //all setting/additions done, persist
+		        context.storage.set(data, function (error) {
+		        	if (error) return cb(error);
+		        	cb(null, "Completed");
+		        });
+		 });
+	}
 }
--- webtasks_drivers/src/drivers/Auth0ExperimentGroceries.java
+++ webtasks_drivers/src/drivers/Auth0ExperimentGroceries.java
@@ -12,6 +12,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 /**
@@ -22,40 +23,109 @@ import com.google.gson.JsonObject;
 public class Auth0ExperimentGroceries {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
+
+		boolean running = true;
+		final BufferedReader cmdReader = new BufferedReader(new InputStreamReader(System.in));
+
 		HttpClient client = HttpClientBuilder.create().build();
 
-		try {
-			HttpPost request = new HttpPost(
-					"https://wt-ec5bd62a56de3fe62cbb4b8ec4ad3f6c-0.sandbox.auth0-extend.com/groceries");
-			JsonObject json = new JsonObject();
-			json.addProperty("name", "Andrew");
-			json.addProperty("other", "blah");
-			System.out.println("JSON: " + json.toString());
-			StringEntity parameters = new StringEntity(json.toString());
-			request.addHeader("content-type", "application/json");
-			request.setEntity(parameters);
+		while (running) {
+			try {
+				System.out.println("Enter LIST, DEL, PUT, or EXIT");
 
-			HttpResponse response = client.execute(request);
+				String commandType = cmdReader.readLine();
 
-			System.out.println("Status: " + response.getStatusLine().getStatusCode());
+				if (commandType.equalsIgnoreCase("EXIT")) {
+					// exit
+					System.out.println("User chose to exit.");
+					running = false;
+					continue;
+				} else if (!commandType.equalsIgnoreCase("LIST") && !commandType.equalsIgnoreCase("DEL")
+						&& !commandType.equalsIgnoreCase("PUT")) {
+					// invalid command
+					System.out.println("Invalid command! Aborting.");
+					continue;
+				}
 
-			String line;
-			StringBuilder result = new StringBuilder();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			while ((line = reader.readLine()) != null) {
-				result.append(line);
+				// command is either LIST or PUT or DEL, possibly mixed case;
+				// throw to upper
+				commandType = commandType.toUpperCase();
+
+				// item list
+				JsonArray jsonItemArray = new JsonArray();
+				// get items for del or put
+				if (commandType.equalsIgnoreCase("DEL") || commandType.equalsIgnoreCase("PUT")) {
+					System.out.println(
+							"You have chosen: [" + commandType + "].\nList all items in comma-separated format.");
+					String itemsString = cmdReader.readLine();
+					itemsString = itemsString.trim();
+					if (itemsString.isEmpty()) {
+						System.out.println("Invalid empty list. Aborting.");
+						continue;
+					} else if (itemsString.equals(",")) {
+						System.out.println("Invalid comma-only list. Aborting.");
+						continue;
+					}
+
+					// convert to array
+					String[] itemsArr = itemsString.split(",");
+
+					// item list
+					jsonItemArray = new JsonArray();
+					for (String item : itemsArr) {
+						item = item.trim();
+						if (item.isEmpty()) {
+							System.out.println("Not adding empty item.");
+						} else {
+							System.out.println("Adding array item: [" + item + "]");
+							jsonItemArray.add(item);
+						}
+					}
+				}
+
+				// request object
+				HttpPost request = new HttpPost(
+						"https://wt-ec5bd62a56de3fe62cbb4b8ec4ad3f6c-0.sandbox.auth0-extend.com/groceries");
+
+				// json to hold all data
+				JsonObject mainJson = new JsonObject();
+				// command type
+				mainJson.addProperty("cmd", commandType);
+				// add list to main json
+				mainJson.add("items", jsonItemArray);
+
+				// final debug log before send
+				System.out.println("JSON: " + mainJson.toString());
+
+				StringEntity parameters = new StringEntity(mainJson.toString());
+				request.addHeader("content-type", "application/json");
+				request.setEntity(parameters);
+
+				HttpResponse response = client.execute(request);
+
+				System.out.println("Status: " + response.getStatusLine().getStatusCode());
+				String line;
+				StringBuilder result = new StringBuilder();
+				BufferedReader responseReader = new BufferedReader(
+						new InputStreamReader(response.getEntity().getContent()));
+				while ((line = responseReader.readLine()) != null) {
+					result.append(line);
+				}
+				System.out.println("Response content: " + result);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			System.out.println("Response content: " + result);
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+
+		System.out.println("Exited");
 	}
 
 }
